3fc3bd06b80f8bd21ea98186d9dd0abebde323cb
Hillel Coren <hillelcoren@gmail.com>
2024-05-31 18:15:10 +0300

    WordPress API

--- app/Controllers/SettingsController.php
+++ app/Controllers/SettingsController.php
@@ -6,38 +6,48 @@
 
 namespace App\Controllers;
 
+use \App\WordPress\SettingsApi;
+
 class SettingsController extends BaseController
 {
-    public function register()
-    {
-        add_action( 'admin_menu', [ $this, 'add_page' ] );
+   public function __construct()
+   {
+      parent::__construct();
 
-        add_filter( 'plugin_action_links_' . $this->plugin_basename, [ $this, 'add_link' ] );  
-    }    
+      $this->settings = new SettingsApi();
+   }
 
-    public function add_page() 
-    {
-       add_menu_page( 
-          'Invoice Ninja', 
-          'Invoice Ninja', 
-          'manage_options', 
-          'invoiceninja', 
-          [ $this, 'render_page' ], 
-          'dashicons-money-alt'
-       );
-    }
+   public function register()
+   {
+      $pages = [
+         [
+            'page_title' => 'Inovice Ninja',
+            'menu_title' => 'Invoice Ninja',
+            'capability' => 'manage_options',
+            'menu_slug' => 'invoiceninja',
+            'callback' => function() { echo '<h1>Plugin</h1>'; },
+            'icon_url' => 'dashicons-money-alt',
+            'position' => 110,
+         ],
+      ];
 
-    public function render_page()
-    {
-       require_once $this->plugin_path . 'templates/settings.php';
-    } 
+      $this->settings->add_pages( $pages )->register();
+
+
+      add_filter( 'plugin_action_links_' . $this->plugin_basename, [ $this, 'add_link' ] );  
+   }    
+
+   public function render_page()
+   {
+      require_once $this->plugin_path . 'templates/settings.php';
+   } 
  
-    public function add_link( $links )
-    {
-       $settings_link = '<a href="admin.php?page=invoiceninja">Settings</a>';
+   public function add_link( $links )
+   {
+      $settings_link = '<a href="admin.php?page=invoiceninja">Settings</a>';
  
-       array_push( $links, $settings_link );
+      array_push( $links, $settings_link );
  
-       return $links;
-    } 
- }
+      return $links;
+   } 
+}
