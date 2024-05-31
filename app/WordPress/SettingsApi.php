a1a714a365b00520538ac2d99d9058a0444164a2
Hillel Coren <hillelcoren@gmail.com>
2024-05-31 18:24:27 +0300

    WordPress API

--- app/WordPress/SettingsApi.php
+++ app/WordPress/SettingsApi.php
@@ -10,6 +10,8 @@ class SettingsApi
 {
     public $admin_pages = [];
 
+    public $admin_subpages = [];
+
     public function register()
     {
         if ( ! empty( $this->admin_pages) ) 
@@ -21,6 +23,31 @@ class SettingsApi
     public function add_pages( array $pages )
     {
         $this->admin_pages = $pages;
+
+        return $this;
+    }
+
+    public function with_subpage( string $title = null )
+    {
+        if ( empty( $this->admin_pages ) ) 
+        {
+            return $this;
+        }
+
+        $admin_page = $this->admin_pages[0];
+
+        $subpages = [
+            [
+                'parent_slug' => $admin_page['menu_slug'],
+                'page_title' => $admin_page['page_title'],
+                'menu_title' => $admin_page['menu_title'],
+                'capability' => $admin_page['capability'],
+                'menu_slug' => $admin_page['menu_slug'],
+                'callback' => $admin_page['callback'],
+            ]                   
+        ];
+
+        $this->admin_subpages = $subpages;
 
         return $this;
     }
@@ -37,6 +64,18 @@ class SettingsApi
                 $page['callback'],
                 $page['icon_url'],
                 $page['position'],
+            );
+        }
+
+        foreach ($this->admin_subpages as $page)
+        {
+            add_submenu_page(
+                $page['prent_slug'],
+                $page['page_title'],
+                $page['menu_title'],
+                $page['capability'],
+                $page['menu_slug'],
+                $page['callback'],
             );
         }
     }
