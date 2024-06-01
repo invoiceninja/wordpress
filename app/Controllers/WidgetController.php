11d0ff2c6b55e880a4874b1b1fbe644bbccf7b21
Hillel Coren <hillelcoren@gmail.com>
2024-06-02 02:14:07 +0300

    Widgets

--- app/Controllers/WidgetController.php
+++ app/Controllers/WidgetController.php
@@ -10,13 +10,6 @@ use \App\Wordpress\Widgets\ProductWidget;
 
 class WidgetController extends BaseController
 {
-    public function __construct()
-    {
-        parent::__construct();
-
-        $this->widgets = new WidgetApi();
-    }
-
     public function register()
     {
         $widget = new ProductWidget();
--- app/WordPress/Widgets/ProductWidget.php
+++ app/WordPress/Widgets/ProductWidget.php
@@ -4,7 +4,7 @@
  * @package Invoice Ninja
  */
 
-namespace App\Widgets;
+namespace App\WordPress\Widgets;
 
 use WP_Widget;
 
@@ -52,33 +52,47 @@ class ProductWidget extends WP_Widget
         register_widget( $this );
     }
 
-    public function widget() 
+    public function widget( $args, $instance ) 
     {
-        
+        echo $args['before_widget'];
+
+        echo "PRODUCTS";
+
+        if ( ! empty( $instance['title'] ) )
+        {
+            echo $args['before_title'] . apply_filter( 'widget_title', $instance['title'] ) . $args['after_title'];
+        }
+
+        echo $args['after_widget'];
     }
 
     public function form( $instance ) 
     {
         $title = ! empty( $instance['title'] ) ? $instance['title'] : esc_html__('Custom Text', 'invoiceninja');
-
-        $titleId = esc_attr( $this->get_field_id( 'title' ));
-
+        $title_id = esc_attr( $this->get_field_id( 'title' ) );
+        $title_name = esc_attr( $this->get_field_name( 'title' ) );
         ?>
 
         <p>
 
-            <label for="<?php echo $titleId ?>">Title</label>
+            <label for="<?php echo $title_id ?>">Title</label>
 
-            <input type="text" class="widefat" id="<?php echo $titleId; ?>" 
-                name="<?php esc_attr( $this->get_field_name( 'title' ) ); ?>" 
+            <input type="text" class="widefat" id="<?php echo $title_id; ?>" 
+                name="<?php echo $title_name; ?>" 
                 value="<?php echo esc_attr( $title ); ?>"/>
+
         </p>
 
         <?php
     }
 
-    public function update() 
+    public function update( $new_intance, $old_instance ) 
     {
-        
+        $instance = $old_instance;
+
+        //$instance['title'] = sanitize_text_field( $new_intance['title'] );
+        $instance['title'] = $new_intance['title'];
+
+        return $instance;
     }
 }
