dea0a20d1e1a460212862ff9ea552bcd89d6ce0f
Hillel Coren <hillelcoren@gmail.com>
2024-06-02 11:46:43 +0300

    Widgets

--- app/WordPress/Widgets/ProductWidget.php
+++ app/WordPress/Widgets/ProductWidget.php
@@ -55,33 +55,45 @@ class ProductWidget extends WP_Widget
 
     public function widget( $args, $instance ) 
     {
-        extract( $args );
+        $query_args = array(
+            'post_type' => 'product',
+            'posts_per_page' => 5,
+        );
+        $query = new \WP_Query($query_args);
 
-        // Check the widget options
-        $title = isset( $instance['title'] ) ? apply_filters( 'widget_title', $instance['title'] ) : '';
-    
-        // WordPress core before_widget hook (always include )
-        echo $before_widget;
-    
-        // Display the widget
-        echo '<div class="widget-text wp_widget_plugin_box">';
+        if ($query->have_posts()) 
+        {
+            extract( $args );
+
+            // Check the widget options
+            $title = isset( $instance['title'] ) ? apply_filters( 'widget_title', $instance['title'] ) : '';
         
-        // Display widget title if defined
-        if ( $title ) {
-            echo $before_title . $title . $after_title;
+            // WordPress core before_widget hook (always include )
+            echo $before_widget;
+        
+            // Display the widget
+            echo '<div class="widget-text wp_widget_plugin_box">';
+            
+            // Display widget title if defined
+            if ( $title ) {
+                echo $before_title . $title . $after_title;
+            }    
+
+            echo '<ul>';
+
+            while ($query->have_posts()) {
+                $query->the_post();
+                echo '<li><a href="' . get_permalink() . '">' . get_the_title() . '</a></li>';
+            }
+
+            echo '</ul>';
+            echo '</div>';
+
+            wp_reset_postdata();    
         }
 
-        echo '<ul>';
-
-        echo '<li><a href="">First Product</a></li>';
-        echo '<li><a href="">Second Product</a></li>';
-
-        echo '</ul>';
-        echo '</div>';
-    
         // WordPress core after_widget hook (always include )
-        echo $after_widget;
-    
+        echo $after_widget;    
     }
 
     public function form( $instance ) 
