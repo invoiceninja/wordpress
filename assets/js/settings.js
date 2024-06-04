d95533ebb29aba66719a5af1cfc60613ce605917
Hillel Coren <hillelcoren@gmail.com>
2024-06-04 13:33:28 +0300

    Storefront

--- assets/js/settings.js
+++ assets/js/settings.js
@@ -9,14 +9,26 @@ window.addEventListener( 'load', function() {
     function switchTab( event ) {
         event.preventDefault();
 
-        document.querySelector( 'ul.nav-tabs li.active' ).classList.remove( 'active' );
-        document.querySelector( '.tab-pane.active' ).classList.remove( 'active' );
+        var active = document.querySelector( 'ul.nav-tabs li.active' );
+        if (active) {
+            active.classList.remove( 'active' );
+        }
 
-        var clickedTab = event.currentTarget;
+        var active = document.querySelector( '.tab-pane.active' );
+        if (active) {
+            active.classList.remove( 'active' );
+        }
+
+        var clickedTab = event.currentTarget;        
+        if (clickedTab) {
+            clickedTab.classList.add( 'active' );
+        }
+
         var anchor = event.target;
         var activePaneId = anchor.getAttribute( 'href' );
-        
-        clickedTab.classList.add( 'active' );
-        document.querySelector( activePaneId ).classList.add( 'active' );
+        var activePane = document.querySelector( activePaneId );
+        if (activePane) {
+            activePane.classList.add( 'active' );
+        }
     }
 });
