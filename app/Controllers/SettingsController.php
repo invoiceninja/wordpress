ce2344dfa75a9e037a6f32d7f1021e00f1d2ebec
Hillel Coren <hillelcoren@gmail.com>
2024-06-04 23:01:25 +0300

    Storefront

--- app/Controllers/SettingsController.php
+++ app/Controllers/SettingsController.php
@@ -199,7 +199,7 @@ class SettingsController extends BaseController
             'callback' => function() 
             { 
                $value = esc_attr( get_option( 'invoiceninja_company_key' ) );
-               echo '<input type="text" class="regular-text code" value="' . $value . '" name="invoiceninja_company_key"/>';
+               echo '<input type="text" class="regular-text code" value="' . $value . '" name="invoiceninja_company_key"' . ( $value ? '' : ' autofocus' ) . '/>';
                echo '<p class="description">Enable the Storefront option on Settings > Client Portal to generate a company key</p>'; 
             },
             'page' => 'invoiceninja_configuration',
