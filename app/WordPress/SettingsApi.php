248d8c11080904242138b0ca914a3c908c7ef022
Hillel Coren <hillelcoren@gmail.com>
2024-06-04 11:11:37 +0300

    Storefront

--- app/WordPress/SettingsApi.php
+++ app/WordPress/SettingsApi.php
@@ -44,6 +44,7 @@ class SettingsApi
     public function enqueueStyles()
     {
         wp_enqueue_style( 'custom-page-styles', plugins_url( '/../../assets/css/settings.css', __FILE__ ) );
+        wp_enqueue_script( 'custom-script', plugins_url( '/../../assets/js/settings.js', __FILE__ ) );        
     }
 
     function optionUpdated($option_name, $old_value, $new_value) 
