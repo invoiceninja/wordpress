b195f9ec5abd129f3a9bac470124403e041e7ef9
Hillel Coren <hillelcoren@gmail.com>
2024-06-13 17:48:09 +0300

    Shopping cart

--- app/InvoiceNinja/InvoiceApi.php
+++ app/InvoiceNinja/InvoiceApi.php
@@ -10,8 +10,20 @@ class InvoiceApi extends BaseApi
 {
     public static function create($cart)
     {
-        $invoice = new \stdClass;
-        $invoice->client_id = 'J0dNxm2aLO';
+        //$invoice = new \stdClass;
+        //$invoice->client_id = 'J0dNxm2aLO';
+
+        $invoice = [
+            'client_id' => 'J0dNxm2aLO',
+            'line_items' => [],
+        ];
+
+        foreach ($cart as $product => $quantity) {
+            $invoice['line_items'][] = [
+                'product_key' => $product,
+                'quantity' => $quantity,
+            ];
+        }
 
         $response = self::sendRequest( 'invoices', 'POST', $invoice );
 
