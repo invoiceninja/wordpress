b4f25314768a57cb61fa7cc23bed17bd82322528
Hillel Coren <hillelcoren@gmail.com>
2024-06-13 17:52:04 +0300

    Shopping cart

--- app/InvoiceNinja/InvoiceApi.php
+++ app/InvoiceNinja/InvoiceApi.php
@@ -18,11 +18,38 @@ class InvoiceApi extends BaseApi
             'line_items' => [],
         ];
 
-        foreach ($cart as $product => $quantity) {
-            $invoice['line_items'][] = [
-                'product_key' => $product,
-                'quantity' => $quantity,
+        foreach ($cart as $product_id => $quantity) {
+
+            $args = [
+                'post_type' => 'invoiceninja_product',
+                'meta_query' => [
+                    [
+                        'key' => 'product_id',
+                        'value' => $product_id,
+                        'compare' => 'EQUAL',
+                    ],
+                ],
             ];
+            
+            $query = new \WP_Query( $args );
+            
+            if ( $query->have_posts() ) {
+                $query->the_post();
+                $post_id = get_the_ID();
+
+                $url = get_permalink();
+                $product = get_the_title();
+                $content = get_the_content();
+                $price = get_post_meta( $post_id, 'price', true );
+
+
+                $invoice['line_items'][] = [
+                    'product_key' => $product,
+                    'quantity' => $quantity,
+                    'cost' => $price,
+                    'notes' => $content,
+                ];
+            }
         }
 
         $response = self::sendRequest( 'invoices', 'POST', $invoice );
