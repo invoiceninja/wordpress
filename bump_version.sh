b4173a86094bcaa70fb957fb5b30ff705f41e4bc
Hillel Coren <hillelcoren@gmail.com>
2024-06-16 19:36:42 +0300

    Build yml

--- bump_version.sh
+++ bump_version.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
-current_version=$(grep "1.0." invoiceninja.php | cut -f3 -d "." )
+current_version=$(grep "0.1." invoiceninja.php | cut -f3 -d "." )
 new_version=$((current_version+1))
 date_today=$(date +%F)
 
 echo "Bump version... $current_version => $new_version"
 
-sed -i -e "s/* Version:            1.0.$current_version/* Version:            1.0.$new_version/g" ./invoiceninja.php
+sed -i -e "s/* Version:            0.1.$current_version/* Version:            0.1.$new_version/g" ./invoiceninja.php
+sed -i -e "s/automatic_release_tag: \"v0.1.$current_version/automatic_release_tag: \"v0.1.$new_version/g" .github/workflows/release.yml
 
 #sed -i -e "s/version: 5.0.$current_version+$current_version/version: 5.0.$new_vesion+$new_vesion/g" ./pubspec.yaml
 #sed -i -e "s/version: 5.0.$current_version+$current_version/version: 5.0.$new_vesion+$new_vesion/g" ./pubspec.foss.yaml
