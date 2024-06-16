12f7f72ca49ec37d9e14fe8ac058b2172d3960ca
Hillel Coren <hillelcoren@gmail.com>
2024-06-16 23:09:22 +0300

    Update version

--- invoiceninja.php
+++ invoiceninja.php
@@ -4,7 +4,7 @@
  * Plugin Name:        Invoice Ninja
  * Plugin URI:         https://github.com/invoiceninja/wordpress
  * Description:        WordPress plugin for Invoice Ninja
- * Version:            0.1.5
+ * Version:            0.1.6
  * Author:             Inovice Ninja
  * Author URI:         https://invoiceninja.com
  * GitHub Plugin URI:  https://github.com/invoiceninja/wordpress
