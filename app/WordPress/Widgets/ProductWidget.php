6c3af029887095b66bed20f393a7cc938719c219
Hillel Coren <hillelcoren@gmail.com>
2024-06-17 22:10:41 +0300

    Working on plugin check errors

--- app/WordPress/Widgets/ProductWidget.php
+++ app/WordPress/Widgets/ProductWidget.php
@@ -71,24 +71,24 @@ class ProductWidget extends WP_Widget
             $title = isset( $instance['title'] ) ? apply_filters( 'widget_title', $instance['title'] ) : '';
         
             // WordPress core before_widget hook (always include )
-            echo esc_attr( $before_widget );
+            echo $before_widget;
         
             // Display the widget
             echo '<div class="widget-text wp_widget_plugin_box">';
             
             // Display widget title if defined
             if ( $title ) {
-                echo esc_attr( $before_title . $title . $after_title );
+                echo $before_title . $title . $after_title;
             }    
 
-            echo esc_attr( '<ul>' );
+            echo '<ul>';
 
             while ($query->have_posts()) {
                 $query->the_post();
-                echo esc_attr( '<li><a href="' . get_permalink() . '">' . get_the_title() . '</a></li>' );
+                echo '<li><a href="' . esc_url( get_permalink() ) . '">' . esc_attr( get_the_title() ) . '</a></li>';
             }
 
-            echo esc_attr( '</ul>' );
+            echo '</ul>';
             echo '</div>';
 
             wp_reset_postdata();    
